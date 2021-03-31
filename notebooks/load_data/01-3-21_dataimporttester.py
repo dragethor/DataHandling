e91ee726e7d272758c33022a5408ba2ae6a83789
Thor Christensen <fisker.christensen@gmail.com>
2021-03-31 12:32:19 +0200

    Somthing went wrong uploading from pycharm...

--- notebooks/load_data/01-3-21_dataimporttester.py
+++
@@ -1,6 +0,0 @@
-from data.read_binary import readDNSdata
-from dotenv import load_dotenv, find_dotenv
-import os
-from memory_profiler import profile
-
-quantities, quanList, xF, yF, zF, length, storl, paraString=readDNSdata('../data/raw/field.0495.u')
